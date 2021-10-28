66bb7457cffb23980fe8c0abf7e8c3ad0644d47c
PhanPhuocDat <1824801030046@tdmu.edu.vn>
2021-10-28 08:08:46 +0700

    working

--- main.js
+++ main.js
@@ -54,3 +54,28 @@ setInterval(function(){
         couter=1;
     }
 },3000);
+
+//CountDown Flash Sale
+const days =document.getElementById('days')
+const hours= document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+// console.log(days)
+const currentYear = new Date().getFullYear();
+const newTime = new Date(`January 1 ${currentYear+2} 00:00:00`)
+
+function updateCountdowntime(){
+    const currentTime = new Date();
+    const diff= newTime - currentTime;
+
+    const d=Math.floor(diff/1000/60/60/24);
+    const h =Math.floor(diff/1000/60/60%24);
+    const m = Math.floor(diff/1000/60)%60;
+    const s = Math.floor(diff/1000)%60;
+    days.innerHTML=d;
+    hours.innerHTML=h<10 ? '0'+h :h;
+    minutes.innerHTML=m<10?'0'+m :m;
+    seconds.innerHTML=s<10 ? '0'+s:s;
+
+}
+setInterval(updateCountdowntime,1000)
